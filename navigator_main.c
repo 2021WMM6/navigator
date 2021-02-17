2e30028463b2558886b488e38b425b60fc5bb4c5
marona42 <marona42@naver.com>
2021-02-17 21:26:25 +0900

    add descriptions

--- navigator_main.c
+++ navigator_main.c
@@ -19,6 +19,7 @@ int main()
     nodelay(stdscr,TRUE);
     keypad(stdscr,TRUE);    //enable keypad, arrows
 
+    //read_tree();    //TODO: 현재 CWD를 기준으로 디렉토리/파일 정보 읽기
     while(1)    //탈출 조건-> q눌렀을때.
     {
         getmaxyx(curscr,termy,termx);     //가로세로 구하기
@@ -26,25 +27,29 @@ int main()
         {
             case KEY_DOWN:
             //TODO: 커서 다운
+            //read_tree();  //옮긴 커서를 기준으로 디렉토리/파일 정보 읽기
             break;
             case KEY_UP:
             //TODO: 커서 업
+            //read_tree();  //옮긴 커서를 기준으로 디렉토리/파일 정보 읽기
             break;
             case KEY_LEFT:
             //TODO: 커서 왼쪽(이전 위치)
+            //read_tree();  //옮긴 커서를 기준으로 디렉토리/파일 정보 읽기
             break;
             case KEY_RIGHT:
             //TODO: 커서 오른쪽(디렉토리 들어가기/동작없음)
+            
             break;
             case KEY_ENTER:
             //TODO: 정보보기?
             break;
-            
         }
         if (ch == EXIT_KEY) //프로그램 종료
             break;
         clear();
-        //TODO: 화면 출력
+
+        //print_tree(); //TODO: 화면 출력
         refresh();
         while((ch=getch())==ERR);   //키 입력 될때까지 대기
     }
