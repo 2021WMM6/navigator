eac4814f5244ca5f45198423444b11f1fda2220a
alex204301 <alex204301@naver.com>
2021-02-25 11:57:07 +0900

    Co-authored-by: marona42 <marona42@users.noreply.github.com> Co-authored-by: jHoon0223 <jHoon0223@users.noreply.github.com> Co-authored-by: JooHui-void <JooHui-void@users.noreply.github.com> Co-authored-by: gwrw1518 <gwrw1518@users.noreply.github.com>

--- test.c
+++ test.c
@@ -16,6 +16,7 @@ char wd[PATH_MAX];
 char dp[PATH_MAX];
 char tp[PATH_MAX];
 char strbuf[PATH_MAX];
+
 typedef struct list{
     char a[PATH_MAX];      //파일 이름
     int type;               //파일 타입
@@ -23,7 +24,10 @@ typedef struct list{
     int clos;               //어떤 디렉토리를 열었을 때 그 디렉토리의 마지막 파일이면 1로 저장, 그 외에는 0으로 저장
     struct list *front;
     struct list *back;
+    char list_change[PATH_MAX];
+    long int list_size;
 }linked;
+struct stat st;
 linked word[10001];
 linked TAIL;
 linked *start = word;
@@ -57,7 +61,7 @@ void save_tree(){
     dirp = opendir(wd);
     while((diritem=readdir(dirp)) != NULL){
         if(strncmp(diritem -> d_name, ".", 1) != 0){
-            sprintf(strbuf,"%s",diritem->d_name);
+            snprintf(strbuf,PATH_MAX,"%s",diritem->d_name);
             temp = diritem -> d_type;
             char *str = strbuf;
             insert_l(str);      //링크드 리스트에 정보를 삽입
@@ -71,6 +75,7 @@ void delete_l(){
     p -> front -> back = p -> back;
     cur = cur -> front;
 }
+
 void insert_l(char *ch){
     linked *p = word + j + 1;
     strcpy(p -> a, ch);
@@ -84,6 +89,7 @@ void insert_l(char *ch){
     j++;
     cur = cur -> back;
 }
+
 void read_tree()
 {
     int i = 0;
@@ -141,14 +147,14 @@ void read_tree()
 }
 void open_dir(){
     cur = cur2;
-    sprintf(dp, "%s/%s", tp, cur -> a);           //dp는 열려고 하는 디렉토리 경로, tp는 현재 디렉토리 경로
+    snprintf(dp, PATH_MAX, "%s/%s", tp, cur -> a);           //dp는 열려고 하는 디렉토리 경로, tp는 현재 디렉토리 경로
     cur -> op = 1;
     DIR *dirp;      //디렉토리 포인터
     struct dirent *diritem;    //디렉토리 항목 포인터
     dirp = opendir(dp);
     while((diritem=readdir(dirp)) != NULL){
         if(strncmp(diritem -> d_name, ".", 1) != 0){        //.으로 시작하는 파일 제외하고 링크드 리스트에 저장
-            sprintf(strbuf,"%s",diritem->d_name);
+            snprintf(strbuf,PATH_MAX,"%s",diritem->d_name);
             temp = diritem -> d_type;
             char *str = strbuf;
             insert_l(str);
@@ -186,7 +192,7 @@ int main()
     getcwd(wd,PATH_MAX);    //현재 디렉토리 가져오기
     save_tree(); //파일 정보 링크드리스트에 저장
     cur2 = end -> front;
-    sprintf(strbuf,"(%s)", cur2 -> a);
+    snprintf(strbuf,PATH_MAX,"(%.4093s)", cur2 -> a);
     strcpy(cur2 -> a, strbuf);
     strcpy(tp, wd);         //wd는 최상위 디렉토리 경로로 바뀌지 않음
     while(1)    //탈출 조건-> x눌렀을때.
@@ -195,7 +201,7 @@ int main()
         lasttime=time(NULL);
         clear();
         //printw("%.*s\n",termx-1, tp);     //현재 경로가 어디인지 출력
-        sprintf(strbuf,"%s",wd);
+        snprintf(strbuf,PATH_MAX,"%s",wd);
         printw("%.*s",termx-1,strbuf);
         switch(ch)
         {
@@ -203,11 +209,11 @@ int main()
             //TODO: 커서 다운
             if(cur2 -> back != &TAIL){
                 if(cur2 -> clos == 0){
-                    sprintf(strbuf, "%s", cur2 -> a + 1);
+                    snprintf(strbuf,PATH_MAX, "%s", cur2 -> a + 1);
                     strncpy(cur2 -> a, strbuf, strlen(cur2 -> a) - 2);
                     cur2 -> a[strlen(cur2 -> a) - 2] = '\0';
                     cur2 = cur2 -> back;
-                    sprintf(strbuf,"(%s)", cur2 -> a);
+                    snprintf(strbuf,PATH_MAX,"(%.4093s)", cur2 -> a);
                     strcpy(cur2 -> a, strbuf);
                 }
             }
@@ -216,11 +222,11 @@ int main()
             //TODO: 커서 업
             if(cur2 -> front != start){
                 if(cur2 -> front -> op == 0){
-                    sprintf(strbuf, "%s", cur2 -> a + 1);
+                    snprintf(strbuf,PATH_MAX, "%s", cur2 -> a + 1);
                     strncpy(cur2 -> a, strbuf, strlen(cur2 -> a) - 2);
                     cur2 -> a[strlen(cur2 -> a) - 2] = '\0';
                     cur2 = cur2 -> front;
-                    sprintf(strbuf,"(%s)", cur2 -> a);
+                    snprintf(strbuf,PATH_MAX,"(%.4093s)", cur2 -> a);
                     strcpy(cur2 -> a, strbuf);
                 }
             }
@@ -229,19 +235,19 @@ int main()
             //TODO: 커서 왼쪽(이전 위치)
             if(cur2 -> front -> op != 0){
                 close_dir();        //디렉토리 닫기
-                sprintf(strbuf,"(%s)", cur2 -> a);
+                snprintf(strbuf,PATH_MAX,"(%.4093s)", cur2 -> a);
                 strcpy(cur2 -> a, strbuf);
             }
             break;
             case KEY_RIGHT:
             //TODO: 커서 오른쪽(디렉토리 들어가기/동작없음)
             if(cur2 -> type == DT_DIR && cur2 -> op == 0){
-                sprintf(strbuf, "%s", cur2 -> a + 1);
+                snprintf(strbuf,PATH_MAX, "%s", cur2 -> a + 1);
                 strncpy(cur2 -> a, strbuf, strlen(cur2 -> a) - 2);
                 cur2 -> a[strlen(cur2 -> a) - 2] = '\0';
                 open_dir();         //디렉토리 열기
                 cur2 = cur2 -> back;
-                sprintf(strbuf,"(%s)", cur2 -> a);
+                snprintf(strbuf,PATH_MAX,"(%.4093s)", cur2 -> a);
                 strcpy(cur2 -> a, strbuf);
             }
             break;
